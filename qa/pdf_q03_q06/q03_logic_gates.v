22bd04cb6057eecac39b3c87a14fd0f79697e21e
kapiltrip <kapiltrip.kapil@gmail.com>
2026-04-19 13:39:00 +0530

    Added explaination to q3

--- qa/pdf_q03_q06/q03_logic_gates.v
+++ qa/pdf_q03_q06/q03_logic_gates.v
@@ -15,3 +15,41 @@ module q03 (
     assign nor_y  = ~(a | b);
     assign xnor_y = ~(a ^ b);
 endmodule
+
+module q03_bitwise_vector #(
+    parameter WIDTH = 4
+) (
+    input  wire [WIDTH-1:0] a,
+    input  wire [WIDTH-1:0] b,
+    output wire [WIDTH-1:0] and_y,
+    output wire [WIDTH-1:0] or_y,
+    output wire [WIDTH-1:0] xor_y,
+    output wire [WIDTH-1:0] nand_y,
+    output wire [WIDTH-1:0] nor_y,
+    output wire [WIDTH-1:0] xnor_y
+);
+    assign and_y  = a & b;
+    assign or_y   = a | b;
+    assign xor_y  = a ^ b;
+    assign nand_y = ~(a & b);
+    assign nor_y  = ~(a | b);
+    assign xnor_y = ~(a ^ b);
+endmodule
+
+module q03_logical (
+    input  wire a,
+    input  wire b,
+    output wire and_y,
+    output wire or_y,
+    output wire xor_y,
+    output wire nand_y,
+    output wire nor_y,
+    output wire xnor_y
+);
+    assign and_y  = a && b;
+    assign or_y   = a || b;
+    assign xor_y  = a != b;
+    assign nand_y = !(a && b);
+    assign nor_y  = !(a || b);
+    assign xnor_y = a == b;
+endmodule
